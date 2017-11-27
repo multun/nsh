60eec456c477b682362106b3d8fd93deb8a9c8ea
Florian Amsallem <amsall_f@epita.fr>
2017-11-27 22:53:14 +0100

    acase: add structure

---
+++ includes/acase.h
@@ -0,0 +1,18 @@
+#pragma-once
+
+#include "wordlist.h"
+
+
+typedef struct acase_node
+{
+  s_wordlist *pattern;
+  struct ast *action;
+  struct acase_node *next;
+} s_acase_node;
+
+
+typedef struct acase
+{
+  s_wordlist *var;
+  s_acase_node *nodes;
+} s_acase;
