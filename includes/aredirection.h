2609ff3628652c5271be6a964b90cff166301b48
Florian Amsallem <amsall_f@epita.fr>
2017-11-27 22:56:48 +0100

    aredirection: add structure

---
+++ includes/aredirection.h
@@ -0,0 +1,17 @@
+#pragma-once
+
+
+typedef struct aredirection
+{
+  enum type
+  {
+    IN,
+    OUT,
+    IN_CONC,
+    OUT_CONC,
+    // TODO
+  } type;
+  int left;
+  wordlist *right;
+  struct ast *action;
+} s_aredirection;
