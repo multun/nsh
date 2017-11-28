710983c9a5ff17cf274535564699f701d30f5fb4
Florian Amsallem <amsall_f@epita.fr>
2017-11-28 19:16:00 +0100

    wordlist: add initializer

--- includes/ast/wordlist.h
+++ includes/ast/wordlist.h
@@ -6,11 +6,14 @@
 typedef struct wordlist
 {
   char *str;
-  size_t size;
   bool split;
   bool expand;
   struct wordlist *next;
 } s_wordlist;
 
 
-s_wordlist *wordlist_create(char *str, size_t size, bool split, bool expand);
+#define WORDLIST(Str, Split, Expand, Next)        \
+  ((s_wordlist)                                   \
+  {                                               \
+    (Str), (Split), (Expand), (Next)              \
+  })
