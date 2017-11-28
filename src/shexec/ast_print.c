98705a735aef8825496a6a31fde035cfdcb5fab7
Florian Amsallem <amsall_f@epita.fr>
2017-11-28 23:09:13 +0100

    ast_print: add for, while, until, bool and list printer functions

--- src/shexec/ast_print.c
+++ src/shexec/ast_print.c
@@ -27,7 +27,7 @@ static void if_print(FILE *f, s_ast *node)
   fprintf(f, "\"%p\" [label=\"IF\"];\n", id);
   print_rec(f, aif->condition);
   void *id_cond = aif->condition;
-  fprintf(f, "\"%p\" -> \"%p\" [label=\"CONDITION\"]\n", id, id_cond);
+  fprintf(f, "\"%p\" -> \"%p\" [label=\"COND\"]\n", id, id_cond);
   print_rec(f, aif->success);
   void *id_succ = aif->success;
   fprintf(f, "\"%p\" -> \"%p\" [label=\"THEN\"]\n", id, id_succ);
@@ -40,6 +40,98 @@ static void if_print(FILE *f, s_ast *node)
 }
 
 
+static void for_print(FILE *f, s_ast *ast)
+{
+  s_afor *afor = &ast->data.ast_for;
+  void *id = ast;
+  fprintf(f, "\"%p\" [label=\"FOR %s in", id, afor->var->str);
+  s_wordlist *wl = afor->collection;
+  while (wl)
+  {
+    fprintf(f, " %s", wl->str);
+    wl = wl->next;
+  }
+  fprintf(f, "\"]\n");
+  print_rec(f, afor->actions);
+  void *id_do = afor->actions;
+  fprintf(f, "\"%p\" -> \"%p\" [label=\"DO\"]\n", id, id_do);
+}
+
+
+static void while_print(FILE *f, s_ast *ast)
+{
+  s_awhile *awhile = &ast->data.ast_while;
+  void *id = ast;
+  fprintf(f, "\"%p\" [label=\"WHILE\"]\n", id);
+
+  print_rec(f, awhile->condition);
+  void *id_cond = awhile->condition;
+  fprintf(f, "\"%p\" -> \"%p\" [label=\"COND\"]\n", id, id_cond);
+
+  print_rec(f, awhile->actions);
+  void *id_do = awhile->actions;
+  fprintf(f, "\"%p\" -> \"%p\" [label=\"DO\"]\n", id, id_do);
+}
+
+
+static void until_print(FILE *f, s_ast *ast)
+{
+  s_auntil *auntil = &ast->data.ast_until;
+  void *id = ast;
+  fprintf(f, "\"%p\" [label=\"UNTIL\"]\n", id);
+
+  print_rec(f, auntil->condition);
+  void *id_cond = auntil->condition;
+  fprintf(f, "\"%p\" -> \"%p\" [label=\"COND\"]\n", id, id_cond);
+
+  print_rec(f, auntil->actions);
+  void *id_do = auntil->actions;
+  fprintf(f, "\"%p\" -> \"%p\" [label=\"DO\"]\n", id, id_do);
+}
+
+
+static void bool_op_print(FILE *f, s_ast *ast)
+{
+  s_abool_op *abool = &ast->data.ast_bool_op;
+  void *id = ast;
+  if (abool->type == BOOL_OR)
+    fprintf(f, "\"%p\" [label=\"OR\"]\n", id);
+  else if (abool->type == BOOL_AND)
+    fprintf(f, "\"%p\" [label=\"AND\"]\n", id);
+  else
+    fprintf(f, "\"%p\" [label=\"NOT\"]\n", id);
+
+  print_rec(f, abool->left);
+  void *id_left = abool->left;
+  fprintf(f, "\"%p\" -> \"%p\"\n", id, id_left);
+
+  if (abool->type != BOOL_NOT)
+  {
+    print_rec(f, abool->right);
+    void *id_right = abool->right;
+    fprintf(f, "\"%p\" -> \"%p\"\n", id, id_right);
+  }
+}
+
+
+static void list_print(FILE *f, s_ast *ast)
+{
+  s_alist *alist = &ast->data.ast_list;
+
+  if (alist)
+  {
+    print_rec(f, alist->action);
+    if (alist->next)
+    {
+      void *id = alist->action;
+      void *id_next = alist->next->action;
+      fprintf(f, "\"%p\" -> \"%p\"\n", id, id_next);
+    }
+    alist = alist->next;
+  }
+}
+
+
 static void print_rec(FILE *f, s_ast *ast)
 {
   if (ast->type == SHNODE_CMD)
@@ -47,11 +139,11 @@ static void print_rec(FILE *f, s_ast *ast)
   else if (ast->type == SHNODE_IF)
     if_print(f, ast);
   else if (ast->type == SHNODE_FOR)
-    return;
+    for_print(f, ast);
   else if (ast->type == SHNODE_WHILE)
-    return;
+    while_print(f, ast);
   else if (ast->type == SHNODE_UNTIL)
-    return;
+    until_print(f, ast);
   else if (ast->type == SHNODE_REDIRECTION)
     return;
   else if (ast->type == SHNODE_PIPE)
@@ -59,9 +151,9 @@ static void print_rec(FILE *f, s_ast *ast)
   else if (ast->type == SHNODE_CASE)
     return;
   else if (ast->type == SHNODE_BOOL_OP)
-    return;
+    bool_op_print(f, ast);
   else if (ast->type == SHNODE_LIST)
-    return;
+    list_print(f, ast);
   else if (ast->type == SHNODE_ASSIGNEMENT)
     return;
   else if (ast->type == SHNODE_FUNCTION)
