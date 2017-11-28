7285393644f85163e9505df6bf637ac2b1d49637
Florian Amsallem <amsall_f@epita.fr>
2017-11-28 19:15:28 +0100

    while: add initializer

--- includes/ast/while.h
+++ includes/ast/while.h
@@ -6,3 +6,10 @@ typedef struct awhile
   struct ast *condition;
   struct ast *actions;
 } s_awhile;
+
+
+#define AWHILE(Condition, Actions)              \
+  ((s_awhile)                                   \
+  {                                             \
+    (Condition), (Actions)                      \
+  })
