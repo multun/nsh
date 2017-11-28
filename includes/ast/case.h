67b36226995b49fa4efd95f66913cf78fc034470
Florian Amsallem <amsall_f@epita.fr>
2017-11-28 19:13:55 +0100

    case: add initializer

--- includes/ast/case.h
+++ includes/ast/case.h
@@ -11,8 +11,22 @@ typedef struct acase_node
 } s_acase_node;
 
 
+#define ACASE_NODE(Pattern, Action, Next)       \
+  ((s_acase_node)                               \
+  {                                             \
+    (Pattern), (Action), (Next)                 \
+  })
+
+
 typedef struct acase
 {
   s_wordlist *var;
   s_acase_node *nodes;
 } s_acase;
+
+
+#define ACASE(Var, Nodes)                       \
+  ((s_acase)                                    \
+  {                                             \
+    (Var), (Nodes)                              \
+  })
