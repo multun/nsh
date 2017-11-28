27c07b09a168acd43fa8f4f8bd39ccbb6654b51a
Florian Amsallem <amsall_f@epita.fr>
2017-11-28 19:14:05 +0100

    cmd: add initializer

--- includes/ast/cmd.h
+++ includes/ast/cmd.h
@@ -7,3 +7,10 @@ typedef struct acmd
 {
   s_wordlist *wordlist;
 } s_acmd;
+
+
+#define ACMD(Wordlist)                          \
+  ((s_wordlist)                                 \
+  {                                             \
+    (Wordlist)                                  \
+  })
