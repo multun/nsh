3a2edd33963a9f289a76ff24281b83b7d4571b72
Florian Amsallem <amsall_f@epita.fr>
2017-11-28 19:15:07 +0100

    pipe: add initializer

--- includes/ast/pipe.h
+++ includes/ast/pipe.h
@@ -5,3 +5,10 @@ typedef struct apipe
   struct ast *left;
   struct ast *right;
 } s_apipe;
+
+
+#define APIPE(Left, Right)                      \
+  ((s_apipe)                                    \
+  {                                             \
+    (Left), (Right)                             \
+  })
