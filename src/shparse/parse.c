61065307397ed8a5ea71ac0c2b4086929ddd917a
JMounier <julien.mounier@epita.fr>
2017-11-28 22:39:28 +0100

    parsing: add rules parsing
    parse_and_or, parse_list

--- src/shparse/parse.c
+++ src/shparse/parse.c
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 #include "shparse/parse.h"
 #include "utils/alloc.h"
 
@@ -18,21 +20,48 @@ s_ast *parse(s_lexer *lexer)
 s_ast *parse_list(s_lexer *lexer)
 {
   s_ast *res = xmalloc(sizeof(s_ast));
+  res->type = SHNODE_LIST;
   res->data.ast_list = ALIST(parse_and_or(lexer), NULL);
   const s_token *tok = lexer_peek(lexer);
   s_alist *tmp = &res->data.ast_list;
   while (tok_is(tok, TOK_SEMI) || tok_is(tok, TOK_AND))
   {
-    s_token *tmp_tok = lexer_pop(lexer);
-    tok_free(tmp_tok);
+    tok_free(lexer_pop(lexer));
     tok = lexer_peek(lexer);
     if (tok_is(tok, TOK_EOF) || tok_is(tok, TOK_NEWLINE))
       return res;
     s_alist *next = xmalloc(sizeof(s_alist));
+    next->type = SHNODE_LIST;
     *next = ALIST(parse_and_or(lexer), NULL);
     // TODO: handle parsing error
     tmp->next = next;
     tmp = next;
+    tok = lexer_peek(lexer);
+  }
+  return res;
+}
+
+s_ast *parse_and_or(s_lexer *lexer)
+{
+  s_ast *res = parse_pipeline(lexer);
+  const s_token *tok = lexer_peek(lexer);
+  while (tok_is(tok, TOK_OR_IF) || tok_is(tok, TOK_AND_IF))
+  {
+    bool or = tok_is(tok, TOK_OR_IF);
+    tok_free(lexer_pop(lexer));
+    tok = lexer_peek(lexer);
+    while (tok_is(tok, TOK_NEWLINE))
+    {
+      tok_free(lexer_pop(lexer));
+      tok = lexer_peek(lexer);
+    }
+    s_ast *bool_op = xmalloc(sizeof(s_ast);
+    bool_op->type = SHNODE_BOOL_OP;
+    bool_op->data.ast_bool_op = ABOOL_OP(or ? BOOL_OR : BOOL_AND,
+                                         res, parse_pipeline(lexer));
+    // TODO: handle parsing exception
+    res = bool_op;
+    tok = lexer_peek(lexer);
   }
   return res;
 }
