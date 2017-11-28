60f2fef2245c9baf4a83b87cd67ed46150417428
Florian Amsallem <amsall_f@epita.fr>
2017-11-28 19:13:08 +0100

    assignement: add initializer

--- includes/ast/assignement.h
+++ includes/ast/assignement.h
@@ -7,3 +7,10 @@ typedef struct aassignement
   s_wordlist *name;
   struct ast *value;
 } s_aassignement;
+
+
+#define AASSIGNEMENT(Name, Value)               \
+  ((s_aassignement)                             \
+  {                                             \
+    (Name), (Value)                             \
+  })
