220768b479820fc36d271211e01c72b71e1b88c2
Florian Amsallem <amsall_f@epita.fr>
2017-11-28 19:14:16 +0100

    for: add initializer

--- includes/ast/for.h
+++ includes/ast/for.h
@@ -9,3 +9,10 @@ typedef struct afor
   s_wordlist *collection;
   struct ast *actions;
 } s_afor;
+
+
+#define AFOR(Var, Collection, Actions)          \
+  ((s_afor)                                     \
+  {                                             \
+    (Var), (Collection), (Actions)              \
+  })
