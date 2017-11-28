11c5a1526891d1789094bfd669230bf7f1624919
Florian Amsallem <amsall_f@epita.fr>
2017-11-28 19:14:56 +0100

    list: add initializer

--- includes/ast/list.h
+++ includes/ast/list.h
@@ -6,3 +6,10 @@ typedef struct alist
   struct ast *action;
   struct alist *next;
 } s_alist;
+
+
+#define ALIST(Action, Next)                     \
+  ((s_alist)                                    \
+  {                                             \
+    (Action), (Next)                            \
+  })
