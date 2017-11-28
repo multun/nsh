a68c8c6b7ad265f76a76f91c213b1ed56d972d2e
Florian Amsallem <amsall_f@epita.fr>
2017-11-28 19:14:29 +0100

    function: add initializer

--- includes/ast/function.h
+++ includes/ast/function.h
@@ -8,3 +8,10 @@ typedef struct afunction
   s_wordlist *name;
   struct ast *value;
 } s_afunction;
+
+
+#define AFUNCTION(Name, Value)                  \
+  ((s_afunction)                                \
+  {                                             \
+    (Name), (Value)                             \
+  })
