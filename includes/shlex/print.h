32add2eeea88b33afae59942da36bc9af5048148
Victor "multun" Collod <victor.collod@epita.fr>
2017-11-30 00:28:27 +0100

    lexer: add token type printer

---
+++ includes/shlex/print.h
@@ -0,0 +1,4 @@
+#include "shlex/lexer.h"
+
+
+const char *token_type_to_string(enum token_type);
---
+++ src/shlex/print.c
@@ -0,0 +1,20 @@
+#include "shlex/lexer.h"
+#include "utils/macros.h"
+
+#define LEX_GEN_TOKS_STR_ENUM(Tokname) #Tokname,
+#define LEX_OPS_STR_ENUM(TokName, Value) #TokName,
+
+
+static const char *g_token_type_tab[] =
+{
+  LEX_OPS(LEX_OPS_STR_ENUM)
+  LEX_GEN_TOKS(LEX_GEN_TOKS_STR_ENUM)
+};
+
+
+const char *token_type_to_string(enum token_type type)
+{
+  if (type > ARR_SIZE(g_token_type_tab))
+    return NULL;
+  return g_token_type_tab[type];
+}
