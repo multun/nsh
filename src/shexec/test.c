e80b11dec26fe7f5bd4d713d7d1f0dc948fa04f9
Florian Amsallem <amsall_f@epita.fr>
2017-11-30 10:22:24 +0100

    test_ast_print: fix using new macros

--- src/shexec/test.c
+++ src/shexec/test.c
@@ -7,8 +7,10 @@
 
 void ast_print_test()
 {
-  s_ast nodecond = AST_ACMD(&WL("$test"));
-  s_ast nodethen = AST_AWHILE(&AST_ACMD(&WL("true")), &AST_ACMD(&WL("echo forever")));
+  s_ast nodecond = AST_ABOOL_OP(BOOL_AND, &AST_ACMD(&WL("$test")),
+                                &AST_ACMD(&WL("$test2")));
+  s_ast nodethen = AST_AWHILE(&AST_ACMD(&WL("true")),
+                              &AST_ACMD(&WL("echo forever")));
   s_ast nodefordo = AST_ACMD(&WL("echo $i"));
   s_ast nodefor = AST_AFOR(&WL("i"), &WL("$(seq 1 10)"), &nodefordo);
   s_ast root = AST_AIF(&nodecond, &nodethen, &nodefor);
