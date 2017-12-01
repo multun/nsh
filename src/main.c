b09c82376dd59bf945b3bf853d1c9a53e366f533
Victor "multun" Collod <victor.collod@epita.fr>
2017-12-01 16:28:39 +0100

    42sh: basic main

--- src/main.c
+++ src/main.c
@@ -1,29 +1,82 @@
+#include "cli/cmdopts.h"
 #include "io/cstream.h"
 #include "shlex/lexer.h"
 #include "shlex/print.h"
+#include "gen/config.h"
 
 #include <stdio.h>
+#include <err.h>
 
 
-int main(int argc, char *argv[])
+static int print_tokens(s_cstream *cs)
 {
-  FILE *f = argc > 1 ? fopen(argv[1], "r") : stdin;
-
-  s_cstream cs;
-  cstream_init_file(&cs, f, argv[1]);
-  s_lexer *lex = lexer_create(&cs);
-  while (!cstream_eof(&cs))
+  s_lexer *lex = lexer_create(cs);
+  while (!cstream_eof(cs))
   {
     s_token *tok = lexer_pop(lex);
     if (!tok)
       break;
 
-    printf("%s(%s)[%c]\n", token_type_to_string(tok->type),
+    printf("%zu:%zu\t%s(%s)[%c]\n", tok->lineinfo.line, tok->lineinfo.column,
+           token_type_to_string(tok->type),
            TOK_STR(tok), tok->delim);
     tok_free(tok, true);
   }
   lexer_free(lex);
-  if (argc > 1)
-    fclose(f);
   return 0;
+}
+
+
+static s_cstream *load_stream(int argc, char *argv[])
+{
+  if (g_cmdopts.src == SHSRC_COMMAND)
+  {
+    if (argc < 1)
+      errx(1, "missing source");
+    return cstream_from_string(argv[0], "<command line>");
+  }
+  else
+  {
+    FILE *f = argc > 0 ? fopen(argv[0], "r") : stdin;
+    return cstream_from_file(f, argv[1]);
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+  int cmdstart = cmdopts_parse(argc, argv);
+  if (cmdstart < 0)
+    // exit when cmdstart < 0,
+    // but succeed when cmdstart == -1
+    return cmdstart + 1;
+
+  argc -= cmdstart;
+  argv += cmdstart;
+
+  if (g_cmdopts.norc)
+    puts("norc");
+
+  s_cstream *cs = load_stream(argc, argv);
+
+  int res = 0;
+  switch (g_cmdopts.shmode)
+  {
+  case SHMODE_VERSION:
+    puts("Version " VERSION);
+    res = 0;
+    break;
+  case SHMODE_AST_PRINT:
+    res = 1;
+    break;
+  case SHMODE_TOKEN_PRINT:
+    res = print_tokens(cs);
+    break;
+  case SHMODE_REGULAR:
+    res = 3;
+    break;
+  }
+
+  cstream_free(cs);
+  return res;
 }
