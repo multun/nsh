95a6ab5b683f03be46a24f3f435621539706d42f
JMounier <julien.mounier@epita.fr>
2017-12-04 23:24:20 +0100

    parsing: add error handling to parse_word.c

--- src/shparse/parse_word.c
+++ src/shparse/parse_word.c
@@ -1,12 +1,18 @@
 #include "utils/alloc.h"
 #include "shparse/parse.h"
+#include "shlex/print.h"
 
 
 s_wordlist *parse_word(s_lexer *lexer, s_errman *errman)
 {
   const s_token *tok = lexer_peek(lexer, errman);
+  if (ERRMAN_FAILING(errman))
+    return NULL;
   if (!tok_is(tok, TOK_WORD))
-    return NULL; // TODO: handle error
+  {
+    sherror(&tok->lineinfo, errman, "unexpected token %s, expected WORD", TOKT_STR(tok));
+    return NULL;
+  }
 
   s_wordlist *res = xmalloc(sizeof(s_wordlist));
   s_token *wrd = lexer_pop(lexer, errman);
