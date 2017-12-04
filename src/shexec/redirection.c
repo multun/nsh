3c71b5f888718281ac94d54cd89239c6c82caffb
Florian Amsallem <amsall_f@epita.fr>
2017-12-04 23:50:01 +0100

    redirection: add '>>' execution

--- src/shexec/redirection.c
+++ src/shexec/redirection.c
@@ -43,16 +43,32 @@ void redirection_print(FILE *f, s_ast *ast)
 }
 
 
+static int fd_save(int fd)
+{
+  int copy = dup(fd);
+  if (copy < 0)
+    errx(1, "42sh: fd_save: Failed dup file descriptor");
+  if (fcntl(copy, F_SETFD, FD_CLOEXEC) < 0)
+    errx(1, "42sh: fd_save: Failed fcntl file descriptor");
+  if (close(fd) < 0)
+    errx(1, "42sh: fd_save: Failed closing file descriptor");
+  return copy;
+}
+
+
+static void fd_load(int copy, int origin)
+{
+  if (dup2(copy, origin) < 0)
+    errx(1, "42sh: fd_load: Failed dup file descriptor");
+
+  if (close(copy) < 0)
+    errx(1, "42sh: fd_load: Failed closing file descriptor");
+}
+
+
 static int redir_great(s_env *env, s_aredirection *aredir, s_ast *cmd)
 {
-  int stdout_copy = dup(STDOUT_FILENO);
-  if (stdout_copy < 0)
-    errx(1, "42sh: redir_great: Failed dup stdout");
-  if (fcntl(stdout_copy, F_SETFD, FD_CLOEXEC) < 0)
-    errx(1, "42sh: redir_great: Failed fcntl stdout_copy");
-
-  if (close(STDOUT_FILENO) < 0)
-    errx(1, "42sh: redir_great: Failed closing stdout");
+  int stdout_copy = fd_save(STDOUT_FILENO);
 
   int fd_file = open(aredir->right->str, O_CREAT | O_WRONLY, 0664);
   if (fd_file < 0)
@@ -70,11 +86,33 @@ static int redir_great(s_env *env, s_aredirection *aredir, s_ast *cmd)
   if (close(fd_file) < 0)
     errx(1, "42sh: redir_great: Failed closing %s", aredir->right->str);
 
-  if (dup2(stdout_copy, STDOUT_FILENO) < 0)
-    errx(1, "42sh: redir_great: Failed dup stdout_copy1");
+  fd_load(stdout_copy, STDOUT_FILENO);
 
-  if (close(stdout_copy) < 0)
-    errx(1, "42sh: redir_great: Failed closing stdout");
+  return res;
+}
+
+
+static int redir_dgreat(s_env *env, s_aredirection *aredir, s_ast *cmd)
+{
+  int stdout_copy = fd_save(STDOUT_FILENO);
+
+  int fd_file = open(aredir->right->str, O_CREAT | O_WRONLY | O_APPEND, 0664);
+  if (fd_file < 0)
+  {
+    warnx("42sh: %s: Permission denied\n", aredir->right->str);
+    return 1;
+  }
+
+  int res = 0;
+  if (aredir->action)
+    res = redirection_exec(env, aredir->action, cmd);
+  else
+    res = ast_exec(env, cmd);
+
+  if (close(fd_file) < 0)
+    errx(1, "42sh: redir_great: Failed closing %s", aredir->right->str);
+
+  fd_load(stdout_copy, STDOUT_FILENO);
 
   return res;
 }
@@ -85,5 +123,7 @@ int redirection_exec(s_env *env, s_ast *ast, s_ast *cmd)
   s_aredirection *aredirection = &ast->data.ast_redirection;
   if (aredirection->type == REDIR_GREAT)
     return redir_great(env, aredirection, cmd);
+  if (aredirection->type == REDIR_DGREAT)
+    return redir_dgreat(env, aredirection, cmd);
   return 0;
 }
