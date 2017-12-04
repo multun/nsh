aaf9c6c01506ad575c99d1217242fb897e840841
JMounier <julien.mounier@epita.fr>
2017-12-04 23:23:55 +0100

    parsing: add error handling to parse_while.c

--- src/shparse/parse_while.c
+++ src/shparse/parse_while.c
@@ -1,4 +1,5 @@
 #include "shparse/parse.h"
+#include "shlex/print.h"
 #include "utils/alloc.h"
 
 s_ast *parse_rule_while(s_lexer *lexer, s_errman *errman)
@@ -6,9 +7,12 @@ s_ast *parse_rule_while(s_lexer *lexer, s_errman *errman)
   tok_free(lexer_pop(lexer, errman), true);
   s_ast *res = xmalloc(sizeof(s_ast));
   res->type = SHNODE_WHILE;
-  res->data.ast_while = AWHILE(parse_compound_list(lexer, errman),
-                               parse_do_group(lexer, errman));
-  // TODO: handle parsing error
+  res->data.ast_while = AWHILE(parse_compound_list(lexer, errman), NULL);
+  if (ERRMAN_FAILING(errman))
+    return res;
+  res->data.ast_while.actions = parse_do_group(lexer, errman);
+  if (ERRMAN_FAILING(errman))
+    return res;
   return res;
 }
 
@@ -17,22 +21,37 @@ s_ast *parse_rule_until(s_lexer *lexer, s_errman *errman)
   tok_free(lexer_pop(lexer, errman), true);
   s_ast *res = xmalloc(sizeof(s_ast));
   res->type = SHNODE_UNTIL;
-  res->data.ast_until = AUNTIL(parse_compound_list(lexer, errman),
-                               parse_do_group(lexer, errman));
-  // TODO: handle parsing error
+  res->data.ast_until = AUNTIL(parse_compound_list(lexer, errman),NULL);
+  if (ERRMAN_FAILING(errman))
+    return res;
+  res->data.ast_until.actions = parse_do_group(lexer, errman);
+  if (ERRMAN_FAILING(errman))
+    return res;
   return res;
 }
 
 s_ast *parse_do_group(s_lexer *lexer, s_errman *errman)
 {
   const s_token *tok = lexer_peek(lexer, errman);
+  if (ERRMAN_FAILING(errman))
+    return NULL;
   if (!tok_is(tok, TOK_DO))
-    return NULL; // TODO: raise unexpected token
+  {
+    sherror(&tok->lineinfo, errman, "unexpected token %s, expected 'do'", TOKT_STR(tok));
+    return NULL;
+  }
   tok_free(lexer_pop(lexer, errman), true);
   s_ast *res = parse_compound_list(lexer, errman);
+  if (ERRMAN_FAILING(errman))
+    return res;
   tok = lexer_peek(lexer, errman);
+  if (ERRMAN_FAILING(errman))
+    return res;
   if (!tok_is(tok, TOK_DONE))
-    return NULL; // TODO: raise unexpected token
+  {
+    sherror(&tok->lineinfo, errman, "unexpected token %s, expected 'done'", TOKT_STR(tok));
+    return NULL;
+  }
   tok_free(lexer_pop(lexer, errman), true);
   return res;
 }
