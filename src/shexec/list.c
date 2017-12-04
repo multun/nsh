62646c94d81c1daf4b63dd658248553eff8a67d9
Florian Amsallem <amsall_f@epita.fr>
2017-12-04 18:21:58 +0100

    list: fix print funciton

--- src/shexec/list.c
+++ src/shexec/list.c
@@ -7,15 +7,14 @@ void list_print(FILE *f, s_ast *ast)
 {
   s_alist *alist = &ast->data.ast_list;
 
+  void *id = ast;
+  fprintf(f, "\"%p\" [label=\"LIST\"];\n", id);
+
   while (alist)
   {
     ast_print_rec(f, alist->action);
-    if (alist->next)
-    {
-      void *id = alist->action;
-      void *id_next = alist->next->action;
-      fprintf(f, "\"%p\" -> \"%p\";\n", id, id_next);
-    }
+    void *id_cur = alist->action;
+    fprintf(f, "\"%p\" -> \"%p\";\n", id, id_cur);
     alist = alist->next;
   }
 }
