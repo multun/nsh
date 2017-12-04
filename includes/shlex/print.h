4e94947d4aa10f56c595d68d9b852bff8ec801e1
JMounier <julien.mounier@epita.fr>
2017-12-04 23:19:27 +0100

    lexer: add TOKT_STR macro to get the printable type of a token

--- includes/shlex/print.h
+++ includes/shlex/print.h
@@ -1,5 +1,6 @@
 #include "shlex/lexer.h"
 
+#define TOKT_STR(Tok) (token_type_to_string(Tok->type))
 
 const char *token_type_to_string(enum token_type);
 int print_tokens(FILE *f, s_cstream *cs, s_errman *errman);
