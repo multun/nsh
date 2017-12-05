b542830380577ed73c4e02b416b349235e353ea0
JMounier <julien.mounier@epita.fr>
2017-12-05 11:14:52 +0100

    parsing: fix linking in rule_if: always link condition and then

--- src/shparse/parse_if.c
+++ src/shparse/parse_if.c
@@ -13,9 +13,9 @@ static bool start_else_clause(const s_token *tok)
 s_ast *parse_rule_if(s_lexer *lexer, s_errman *errman)
 {
   tok_free(lexer_pop(lexer, errman), true);
-  s_ast *res = xmalloc(sizeof(s_ast));
+  s_ast *res = xcalloc(sizeof(s_ast), 1);
   res->type = SHNODE_IF;
-  s_ast *cond = parse_compound_list(lexer, errman);
+  res->data.ast_if.condition = parse_compound_list(lexer, errman);
   if (ERRMAN_FAILING(errman))
     return res;
   const s_token *tok = lexer_peek(lexer, errman);
@@ -23,14 +23,14 @@ s_ast *parse_rule_if(s_lexer *lexer, s_errman *errman)
     return res;
   if (!tok_is(tok, TOK_THEN))
   {
-    sherror(&tok->lineinfo, errman, "unexpected token %s, expected 'then'", TOKT_STR(tok));
+    sherror(&tok->lineinfo, errman,
+            "unexpected token %s, expected 'then'", TOKT_STR(tok));
     return res;
   }
   tok_free(lexer_pop(lexer, errman), true);
-  s_ast *then = parse_compound_list(lexer, errman);
+  res->data.ast_if.success = parse_compound_list(lexer, errman);
   if (ERRMAN_FAILING(errman))
     return res;
-  res->data.ast_if = AIF(cond, then, NULL);
   tok = lexer_peek(lexer, errman);
   if (ERRMAN_FAILING(errman))
     return res;
@@ -41,7 +41,9 @@ s_ast *parse_rule_if(s_lexer *lexer, s_errman *errman)
   }
   else if (!start_else_clause(tok))
   {
-    sherror(&tok->lineinfo, errman, "unexpected token %s, expected 'fi', 'else' or 'elif'", TOKT_STR(tok));
+    sherror(&tok->lineinfo, errman,
+            "unexpected token %s, expected 'fi', 'else' or 'elif'",
+            TOKT_STR(tok));
     return res;
   }
   res->data.ast_if.failure = parse_else_clause(lexer, errman);
@@ -52,7 +54,8 @@ s_ast *parse_rule_if(s_lexer *lexer, s_errman *errman)
     return res;
   if (!tok_is(tok, TOK_FI))
   {
-    sherror(&tok->lineinfo, errman, "unexpected token %s, expected 'fi'", TOKT_STR(tok));
+    sherror(&tok->lineinfo, errman,
+            "unexpected token %s, expected 'fi'", TOKT_STR(tok));
     return  res;
   }
   tok_free(lexer_pop(lexer, errman), true);
@@ -68,9 +71,9 @@ s_ast *parse_else_clause(s_lexer *lexer, s_errman *errman)
   tok_free(lexer_pop(lexer, errman), true);
   if (!elif)
     return parse_compound_list(lexer, errman);
-  s_ast *res = xmalloc(sizeof(s_ast));
+  s_ast *res = xcalloc(sizeof(s_ast), 1);
   res->type = SHNODE_IF;
-  s_ast *cond = parse_compound_list(lexer, errman);
+  res->data.ast_if.condition = parse_compound_list(lexer, errman);
   if (ERRMAN_FAILING(errman))
     return res;
   tok = lexer_peek(lexer, errman);
@@ -78,18 +81,19 @@ s_ast *parse_else_clause(s_lexer *lexer, s_errman *errman)
     return res;
   if (!tok_is(tok, TOK_THEN))
   {
-    sherror(&tok->lineinfo, errman, "unexpected token %s, expected 'then'", TOKT_STR(tok));
+    sherror(&tok->lineinfo, errman,
+            "unexpected token %s, expected 'then'", TOKT_STR(tok));
     return res;
   }
   tok_free(lexer_pop(lexer, errman), true);
-  s_ast *then = parse_compound_list(lexer, errman);
+  res->data.ast_if.success = parse_compound_list(lexer, errman);
   if (ERRMAN_FAILING(errman))
     return res;
-  res->data.ast_if = AIF(cond, then, NULL);
   tok = lexer_peek(lexer, errman);
   if (!start_else_clause(tok))
   {
-    sherror(&tok->lineinfo, errman, "unexpected token %s, expected 'else' or 'elif'", TOKT_STR(tok));
+    sherror(&tok->lineinfo, errman,
+            "unexpected token %s, expected 'else' or 'elif'", TOKT_STR(tok));
     return res;
   }
   res->data.ast_if.failure = parse_else_clause(lexer, errman);
