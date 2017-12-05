6dc310cc4897dfbb0fe31e3dd079bdbc3f7462dd
Victor "multun" Collod <victor.collod@epita.fr>
2017-12-05 10:52:56 +0100

    tests: print the name of the mismatched property

--- tests/check.py
+++ tests/check.py
@@ -92,16 +92,28 @@ def run_test(conf, stdin):
                              run_process(conf, command_ref, stdin))
 
 
+def format_test(conf, test_path, test_input):
+    print(f'Currently running {test_path.name}...', end='')
+    res = run_test(args, test_input)
+    success = not any(comp.obj for comp in res)
+    tag = highlight('[OK]' if success else '[KO]', success, True)
+    print(f"\x1b[2K\r{tag}\t{test_path.name}")
+    if not success:
+        for comp in (comp for comp in res if comp.obj):
+            failmsg = f'>>> mismatched {comp.name} <<<'
+            print(highlight(failmsg, False, True))
+            print(highlight('<' * len(failmsg), False, True))
+            print(highlight(comp.obj, success, True))
+
+
 if __name__ == '__main__':
     args = _test_parser().parse_args()
-    for suite_path, suite_tests in discover_integration_tests(args):
+    tests = discover_integration_tests(args)
+    if args.list:
+        for suite_name in (suite.name for suite, _ in tests):
+            print(suite_name)
+        exit(0)
+    for suite_path, suite_tests in tests:
         print(f'\t>> {highlight(suite_path.name, True, False)} <<', end='\n\n')
         for test_path, test_input in suite_tests:
-            print(f'Currently running {test_path.name}...', end='')
-            res = run_test(args, test_input)
-            success = not any(comp.obj for comp in res)
-            tag = highlight('[OK]' if success else '[KO]', success, True)
-            print(f"\x1b[2K\r{tag}\t{suite_path.name} -> {test_path.name}")
-            if not success:
-                for comp in res:
-                    print(highlight(comp.obj, success, True))
+            format_test(args, test_path, test_input)
