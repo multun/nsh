97c56237b41258e1e2854464bb700ee602f4a1ed
Victor "multun" Collod <victor.collod@epita.fr>
2017-12-09 23:46:38 +0100

    shexec: clean_exit: implement clean_err

--- includes/shexec/clean_exit.h
+++ includes/shexec/clean_exit.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include "utils/attr.h"
 #include "utils/error.h"
-
 
 extern s_ex_class g_clean_exit;
 
+void ATTR(noreturn) clean_err(s_errcont *cont, int retcode,
+                              const char *fmt, ...);
 void clean_exit(s_errcont *cont, int retcode);
--- src/shexec/clean_exit.c
+++ src/shexec/clean_exit.c
@@ -1,9 +1,25 @@
 #include "shexec/clean_exit.h"
 
+#include <err.h>
+#include <errno.h>
+#include <stdarg.h>
+
 s_ex_class g_clean_exit;
 
 
-void clean_exit(s_errcont *cont, int retcode)
+void ATTR(noreturn) clean_err(s_errcont *cont, int retcode, const char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+
+  vwarn(fmt, ap);
+  clean_exit(cont, retcode);
+
+  va_end(ap);
+}
+
+
+void ATTR(noreturn) clean_exit(s_errcont *cont, int retcode)
 {
   cont->errman->retcode = retcode;
   shraise(cont, &g_clean_exit);
