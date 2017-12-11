c8eca9a0b87b9290a6d94ab3c10c544280c123f2
JMounier <julien.mounier@epita.fr>
2017-12-11 21:26:23 +0100

    utils: fix strsplit to iterate on token array

--- src/utils/strsplit.c
+++ src/utils/strsplit.c
@@ -2,47 +2,20 @@
 #include <stdbool.h>
 #include <string.h>
 
-static size_t is_delim(const char **delim, size_t nb_delim, char *c)
+static bool is_delim(const char **delim, size_t nb_delim, char *str)
 {
   for (size_t i = 0; i < nb_delim; i++)
-    if (!strncmp(delim[i], c, strlen(delim[i])))
-      return strlen(delim[i]);
-  return 0;
+    if (!strncmp(delim[i], str, strlen(delim[i])))
+      return true;
+  return false;
 }
 
 
-static char *strsplit_r_sub(char **str, const char **delim, size_t nb_delim)
+size_t strsplit_r(char **str, const char **delim, size_t nb_delim)
 {
-  char *beg = *str;
-  size_t i = 0;
-  while (!(i = is_delim(delim, nb_delim, *str)))
-    if (!**str)
-    {
-      *str = NULL;
-      return beg;
-    }
-    else
-      (*str)++;
-
-  if (!**str)
-    return NULL;
-  **str = '\0';
-  (*str) += i;
-  return beg;
-}
-
-
-char* strsplit_r(char *str, const char **delim,
-                 size_t nb_delim, char **saveptr)
-{
-  if (str)
-    *saveptr = str;
-
-  char *ret;
-
-  do {
-    if (!delim || !saveptr || !*saveptr || !**saveptr)
-      return NULL;
-  } while ((ret = strsplit_r_sub(saveptr, delim, nb_delim)) && *ret == '\0');
-  return ret;
+  size_t res = 0;
+  for (; str[res]; res++)
+    if (is_delim(delim, nb_delim, str[res]))
+      return res;
+  return res;
 }
