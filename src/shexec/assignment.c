d436da557b13879f0cdbc372f55cd7cf4596db32
Florian Amsallem <amsall_f@epita.fr>
2017-12-15 17:02:02 +0100

    assignment: fix wrong variable

--- src/shexec/assignment.c
+++ src/shexec/assignment.c
@@ -1,5 +1,8 @@
+#include <err.h>
+#include <regex.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
 
 #include "ast/ast.h"
 #include "shexp/expansion.h"
@@ -45,6 +48,23 @@ int assignment_exec(s_env *env, s_ast *ast, s_ast *cmd, s_errcont *cont)
     return ast_exec(env, cmd, cont);
   char *name = strdup(ast->data.ast_assignment.name->str);
   char *value = expand(ast->data.ast_assignment.value->str, env, cont);
+
+  regex_t regex;
+  if (regcomp(&regex, "^[[:alpha:]_][[:alnum:]_]*$", REG_EXTENDED))
+  {
+    warnx("export: an error occurs compiling the regex");
+    return 1;
+  }
+  if (regexec(&regex, name, 0, NULL, 0) == REG_NOMATCH)
+  {
+    warnx("assignment: '%s': not a valid identifier", name);
+    regfree(&regex);
+    free(value);
+    free(name);
+    return 127;
+  }
+  regfree(&regex);
+
   assign_var(env, name, value, cmd != NULL);
   return assignment_exec(env, ast->data.ast_assignment.action,
                            cmd, cont);
