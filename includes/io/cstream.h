fb21c6ef9bb13faca6c3115a3d333c2bbc0cd289
Victor "multun" Collod <victor.collod@epita.fr>
2017-12-16 23:54:46 +0100

    io: properly document interfaces

--- includes/io/cstream.h
+++ includes/io/cstream.h
@@ -9,12 +9,23 @@
 struct cstream;
 struct context;
 
+
+/**
+** \brief A pointer to a function able to read from a specific stream type
+** \param a stream to pull data from
+** \return a character, or EOF
+*/
 typedef int (*f_io_reader)(struct cstream *cs);
+
+/**
+** \brief A pointer to a function destructing a specific stream type instance
+** \param a stream to destruct
+*/
 typedef void (*f_io_destructor)(struct cstream *cs);
 
 
 /**
-** \brief io backends are generic to avoid code duplication
+** \brief io backends are structures holding stream type specific routines
 */
 typedef struct io_backend
 {
@@ -29,33 +40,109 @@ typedef struct io_backend
 */
 typedef struct cstream
 {
-  void *data;
+  // whether the stream is interactive
   bool interactive;
-  bool exit_close;
+
+  // the context the stream runs in. This is useful for getting the prompt,
+  // as well as the aliasing context
   struct context *context;
+
   s_io_backend *backend;
+  // the line-related informations, updated on each read
+  s_lineinfo line_info;
+
+  // whether buf contains some data
+  bool has_buf;
+
+  // a buffer enabling the user to peek data from the stream
+  int buf;
+
+  // whether the stream reached EOF
+  bool eof;
+
+  /*
+  ** Backend-specific data
+  */
+
+  // contains each poped character since the beginning of the line.
+  // it might seem useless or awkward to have this here, but the current
+  // readline backend needs it
+  s_evect linebuf;
+
+  // should the stream be closed on exit ? only meaningful for the file backend
+  bool exit_close;
+
   // a backend-dependant cursor
   size_t back_pos;
-  // the line-related informations
-  s_lineinfo line_info;
-  // whether a read character is waiting to be poped
-  bool has_buf;
-  int buf;
-  bool eof;
-  // contains each poped character
-  s_evect linebuf;
+
+  // a backend specific data pointer
+  void *data;
 } s_cstream;
 
 
+/**
+** \brief constructs a base stream, initialized with sane default values
+** \desc using this interface ensures destructing the stream does not exploit
+**    undefined behavior
+** \return a partialy uninitialized stream
+*/
 s_cstream *cstream_create_base(void);
+
+
+/**
+** \brief initializes a stream from a file
+** \param source the source to display on error
+** \param exit_close whether to close the stream on error
+** \return a file stream
+*/
 s_cstream *cstream_from_file(FILE *stream, const char *source, bool exit_close);
+
+
+/**
+** \brief initializes a readline stream
+** \return a readline stream
+*/
 s_cstream *cstream_readline(void);
+
+
+/**
+** \brief initializes a stream from a string
+** \param source the source to display on error
+** \return a string stream
+*/
 s_cstream *cstream_from_string(char *string, char *source);
 
+
+/**
+** \brief destructs a stream and all the underlying resources
+** \param cs the stream to destroy
+*/
 void cstream_free(s_cstream *cs);
 
+
+/**
+** \brief peek a character from a stream
+** \desc returns without removing the next character of the stream
+**   the second call in a row to peek always returns the same character
+** \param cs the stream to peek from
+** \return the next character on the stream
+*/
 int cstream_peek(s_cstream *cs);
+
+
+/**
+** \brief pop a character from a stream
+** \desc returns and removes a character from the stream
+** \param cs the stream to peek from
+** \return the next character on the stream
+*/
 int cstream_pop(s_cstream *cs);
+
+
+/**
+** \param cs the stream involved
+** \return has the stream reached eof?
+*/
 bool cstream_eof(s_cstream *cs);
 
 
--- includes/io/managed_stream.h
+++ includes/io/managed_stream.h
@@ -7,6 +7,13 @@
 
 struct arg_context;
 
-
+/**
+** \brief initializes a stream according to a command line and a context
+** \param context the context associated with the stream
+** \param cs the pointer to store the result in
+** \param arg_cont the command line to read from
+** \return the status code of the operation.
+**    if non zero, the program shall exit
+*/
 int cstream_dispatch_init(struct context *context, s_cstream **cs,
                           struct arg_context *arg_cont);
