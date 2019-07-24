80fb8c46a1a2d69383b5f56acd526e348828fb9e
Victor multun Collod <victor.collod@epita.fr>
2019-07-24 23:51:57 +0200

    nix: add debugging utils

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "dish";
   buildInputs = [ readline ];
-  nativeBuildInputs = [ python3 jq gdb gcc autoconf automake libtool ];
+  nativeBuildInputs = [ python3 jq gdb gcc autoconf automake libtool valgrind bear ];
   # not suitable for debug builds
   hardeningDisable = [ "fortify" ];
 }
