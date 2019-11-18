35ff7d9efb2d1f2a7b5a08b776d648be8847f965
Victor multun Collod <victor.collod@epita.fr>
2019-11-18 18:56:27 +0100

    shexec: fix break error handling

--- src/shexec/builtins/break.c
+++ src/shexec/builtins/break.c
@@ -8,7 +8,7 @@
 static int builtin_generic_break(struct environment *env, int argc, char **argv)
 {
     if (argc > 2) {
-        warnx("%s: too many arguments", argv[0]);
+        fprintf(stderr, "%s: too many arguments\n", argv[0]);
         return 1;
     }
 
@@ -19,26 +19,32 @@ static int builtin_generic_break(struct environment *env, int argc, char **argv)
         env->break_count = 1;
     else {
         unsigned long int res = strtoul(argv[1], NULL, 10);
-        if (res == ULONG_MAX || res == 0 || res > INT_MAX) {
-            warnx("%s: invalid break count \"%s\"", argv[0], argv[1]);
+        if (res == ULONG_MAX || res == 0) {
+            fprintf(stderr, "%s: invalid break count \"%s\"\n", argv[0], argv[1]);
             return 1;
         }
+
+        /* clamp to INT_MAX to make the convertion safe */
+        if (res > INT_MAX)
+            res = INT_MAX;
+
         env->break_count = res;
     }
 
-    /* TODO: this is posix-correct but deserves a warning */
+    /* TODO: this is required by posix but deserves a warning */
     if (env->break_count > env->depth)
         env->break_count = env->depth;
 
     env->code = 0;
-    return 0;
+    /* the special -1 return code tells the wrapper to raise an exception */
+    return -1;
 }
 
 int builtin_break(struct environment *env, struct errcont *cont,
                   int argc, char **argv)
 {
     int rc;
-    if ((rc = builtin_generic_break(env, argc, argv)))
+    if ((rc = builtin_generic_break(env, argc, argv)) >= 0)
         return rc;
     shraise(cont, &g_ex_break);
 }
@@ -47,7 +53,7 @@ int builtin_continue(struct environment *env, struct errcont *cont,
                      int argc, char **argv)
 {
     int rc;
-    if ((rc = builtin_generic_break(env, argc, argv)))
+    if ((rc = builtin_generic_break(env, argc, argv)) >= 0)
         return rc;
     shraise(cont, &g_ex_continue);
 }
