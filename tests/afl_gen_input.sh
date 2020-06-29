a9e5e55cd9ab305ab0c5c18135ace46ccb3e4860
Victor "multun" Collod <victor.collod@epita.fr>
2020-06-28 18:19:46 -0700

    tests: afl_gen_input.sh: add afl-cmin support

--- tests/afl_gen_input.sh
+++ tests/afl_gen_input.sh
@@ -3,13 +3,23 @@
 set -e
 
 testdir="$(dirname "$0")"
+rootdir="$testdir"/..
+chroot="$rootdir"/chroot
 
 export output_dir="${1:?missing output dir}"
+export cmin_dir="${2:?missing minimized subset dir}"
+export chroot
 
-mkdir -p -- "$output_dir"
+mkdir -p -- "${chroot}/${output_dir}" "${chroot}/${cmin_dir}"
 
 find "$testdir" -name '*.json' -exec sh -c '
      set -e
      test_name="$(basename "$1" .json)"
-     jq -r .stdin "$1" > "${output_dir}/${test_name}"
+     jq -r .stdin "$1" > "${chroot}/${output_dir}/${test_name}"
 ' -- {} \;
+
+"$testdir"/run_chroot.sh \
+    afl-cmin \
+    -i "${output_dir}" \
+    -o "${cmin_dir}" \
+    ./nsh @@ cmin_dir
