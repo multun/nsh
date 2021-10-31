22327c62e6603481b7607cf4ed115f7ba35efa51
Victor "multun" Collod <victor.collod@epita.fr>
2021-11-01 00:49:24 +0100

    nix: add pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -2,11 +2,27 @@
   description = "A POSIX shell you can read the source of";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  inputs.flake-utils.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in
       rec {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixpkgs-fmt.enable = true;
+              clang-format = {
+                enable = true;
+                types_or = [ "c" ];
+              };
+            };
+          };
+        };
         packages = flake-utils.lib.flattenTree {
           nsh = pkgs.stdenv.mkDerivation {
             pname = "nsh";
@@ -20,18 +36,19 @@
         apps.nsh = flake-utils.lib.mkApp { drv = packages.nsh; };
         defaultApp = apps.nsh;
         devShell = pkgs.mkShell {
-            # toolchain hardening injects unwanted compiler flags.
-            # fortify injects -O2 along with -D_FORTIFY_SOURCE=2,
-            # which breaks the debugging experience
-            hardeningDisable = [ "all" ];
-            inputsFrom = [ packages.nsh ];
-            packages = [
-              pkgs.python3
-              pkgs.doxygen
-              pkgs.gdb
-              pkgs.afl
-              pkgs.valgrind
-            ];
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          # toolchain hardening injects unwanted compiler flags.
+          # fortify injects -O2 along with -D_FORTIFY_SOURCE=2,
+          # which breaks the debugging experience
+          hardeningDisable = [ "all" ];
+          inputsFrom = [ packages.nsh ];
+          packages = [
+            pkgs.python3
+            pkgs.doxygen
+            pkgs.gdb
+            pkgs.afl
+            pkgs.valgrind
+          ];
         };
       }
     );
