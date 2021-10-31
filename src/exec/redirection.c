4dfffde68ae5d0b1a3a91adf09288cfdaa656936
Victor "multun" Collod <victor.collod@epita.fr>
2021-10-31 21:41:57 +0100

    exec: better comment the redir code

--- src/exec/redirection.c
+++ src/exec/redirection.c
@@ -10,13 +10,20 @@
 #include <nsh_utils/macros.h>
 #include <nsh_exec/execution.h>
 #include <nsh_utils/strutils.h>
+#include <nsh_utils/logging.h>
 
-static int fd_copy(int fd)
+
+/**
+** Try to make a copy, or return -1 if the file descriptor doesn't exist
+*/
+static int fd_try_copy(int fd)
 {
     // store the copied fd somewhere inaccessible
     int copy = fcntl(fd, F_DUPFD, 10);
     if (copy == -1)
         return copy;
+
+    nsh_info("made a copy of %d (%d)", fd, copy);
 
     // TODO: save fd params with GETFD, see strace bash
     if (fcntl(copy, F_SETFD, FD_CLOEXEC) < 0)
@@ -24,30 +31,31 @@ static int fd_copy(int fd)
     return copy;
 }
 
-static int fd_move_away(int fd)
+static int fd_try_move_away(int fd)
 {
-    int copy = fd_copy(fd);
+    nsh_info("moving away %d", fd);
+    int copy = fd_try_copy(fd);
     if (copy == -1)
-        return copy;
+        return -1;
 
+    nsh_info("closing %d", fd);
     if (close(fd))
-        err(1, "fd_move_away: close failed");
+        err(1, "fd_try_move_away: close failed");
     return copy;
 }
 
 static int fd_move_close(int src, int dst)
 {
+    nsh_info("moving %d into %d, and closing %d", src, dst, src);
     if (src == dst)
         return 0;
 
-    if (dup2(src, dst) == -1)
-    {
+    if (dup2(src, dst) == -1) {
         warn("fd_move_close: dup2 failed");
         return 1;
     }
 
-    if (close(src) == -1)
-    {
+    if (close(src) == -1) {
         warn("fd_move_close: close failed");
         return 1;
     }
@@ -57,12 +65,11 @@ static int fd_move_close(int src, int dst)
 static int fd_open_into(int dst, const char *path, int flags, mode_t mode)
 {
     int fd_file = open(path, flags, mode);
-    if (fd_file == -1)
-    {
+    if (fd_file == -1) {
         warn("open('%s') failed", path);
         return -1;
     }
-
+    nsh_info("opened %s, got fd %d. Moving to fd %d", path, fd_file, dst);
     fd_move_close(fd_file, dst);
     return 0;
 }
@@ -77,6 +84,7 @@ static struct redir_undo_op *redir_undo_reserve(struct redir_undo *undo)
 
 static void redir_undo_plan_close(struct redir_undo *undo, int to_close)
 {
+    assert(to_close > 0);
     struct redir_undo_op *op = redir_undo_reserve(undo);
     op->type = REDIR_CLOSE;
     op->data.to_close = to_close;
@@ -84,9 +92,10 @@ static void redir_undo_plan_close(struct redir_undo *undo, int to_close)
 
 static void redir_undo_plan_restore(struct redir_undo *undo, int src, int dst)
 {
-    // if the copy is -1, plan a close instead
-    if (src == -1)
-    {
+    // when we're planning to restore -1,
+    // it means there was no file descriptor to restore, and thus that
+    // we can instead close dst
+    if (src == -1) {
         redir_undo_plan_close(undo, dst);
         return;
     }
@@ -108,7 +117,7 @@ static int redir_simple(struct shast_redirection *redir,
         src = default_src;
 
     /* make a copy of it */
-    int src_copy = fd_move_away(src);
+    int src_copy = fd_try_move_away(src);
 
     /* open into the choosen file descriptor */
     int rc = fd_open_into(src, shword_buf(redir->right), open_flags, 0664);
@@ -147,7 +156,7 @@ static int redir_lessgreat(struct shast_redirection *redir,
 // [n]>&-
 static int redir_close(struct redir_undo *undo, int fd)
 {
-    int copy = fd_move_away(fd);
+    int copy = fd_try_move_away(fd);
     if (copy == -1) {
         warn("close failed");
         return 1;
@@ -156,7 +165,9 @@ static int redir_close(struct redir_undo *undo, int fd)
     return 0;
 }
 
-// [n]>&fd
+
+// [n]>&word or [n]<&word (> makes n default to stdout, < to stdin)
+// basicaly means fd(n) = fd[word], or dup2(word, n)
 static int redir_dup(struct shast_redirection *redir,
                      struct redir_undo *undo,
                      int default_fd)
@@ -165,6 +176,8 @@ static int redir_dup(struct shast_redirection *redir,
     if (left == -1)
         left = default_fd;
 
+    // if word evaluates to '-', file descriptor n,
+    // or standard output if n is not specified, is closed
     if (strcmp(shword_buf(redir->right), "-") == 0)
         return redir_close(undo, left);
 
@@ -175,10 +188,15 @@ static int redir_dup(struct shast_redirection *redir,
     }
 
     // make a save of the left file descriptor
-    int left_copy = fd_copy(left);
+    int left_copy = fd_try_copy(left);
+    if (left_copy == -1)
+        nsh_info("making a copy of %d failed", left);
+
     // duplicate the right fd into the left one (that's why a save was made)
+    nsh_info("duplicating %d into %d", right, left);
     if (dup2(right, left) == -1) {
-        warnx("%s: Bad file descriptor", shword_buf(redir->right));
+        // TODO: handle closing left_copy
+        warn("failed to dup %d", right);
         return 1;
     }
 
@@ -233,13 +251,14 @@ int redirection_exec(struct shast_redirection *redir, struct redir_undo *undo)
 
 int redirection_op_cancel(struct redir_undo_op *undo_op)
 {
+    nsh_info("undoing redirection:");
     switch (undo_op->type)
     {
     case REDIR_RESTORE:
         return fd_move_close(undo_op->data.move.src, undo_op->data.move.dst);
     case REDIR_CLOSE:
-        if (close(undo_op->data.to_close) == -1)
-        {
+        nsh_info("closing %d", undo_op->data.to_close);
+        if (close(undo_op->data.to_close) == -1) {
             warn("failed closing file descriptor in redirection cleanup");
             return 1;
         }
